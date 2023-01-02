9d976c3a376f35cdafa84be75f6636bb7a5ab746
Steph Crown <emmanuelstephen024@gmail.com>
2023-01-02 17:54:57 +0100

    Create external link component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,3 @@
-export { default as Button } from "./Button";
 export { default as Header } from "./Header";
 export { default as IssuesTable } from "./IssuesTable";
 export { default as IssuesTableHead } from "./IssuesTableHead";
