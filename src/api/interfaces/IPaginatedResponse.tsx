86fa35d07b0bc8f31ce39123f4462d7c9fea5bc3
Steph Crown <emmanuelstephen024@gmail.com>
2023-01-02 20:00:37 +0100

    Revert "Transform issues response from api"
    This reverts commit 8270c15280fc7d48873389f26f502648ae0d9ed9.

--- src/api/interfaces/IPaginatedResponse.tsx
+++
@@ -1,7 +0,0 @@
-interface IPaginatedResponse<T> {
-  total_count: number;
-  incomplete_results: boolean;
-  items: T;
-}
-
-export default IPaginatedResponse;
--- src/api/interfaces/index.tsx
+++ src/api/interfaces/index.tsx
@@ -1,6 +1,5 @@
 export type { default as IIssue } from "./IIssue";
 export type { default as ILabel } from "./ILabel";
-export type { default as IPaginatedResponse } from "./IPaginatedResponse";
 export type { default as IPullRequest } from "./IPullRequest";
 export type { default as IReactions } from "./IReactions";
 export type { default as IUser } from "./IUser";
