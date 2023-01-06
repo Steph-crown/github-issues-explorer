0b15fc9661380de3792fae082a2d41c17a473f44
Steph Crown <emmanuelstephen024@gmail.com>
2023-01-06 08:28:38 +0100

    Create portal component

---
+++ src/components/Portal/index.tsx
@@ -0,0 +1,32 @@
+import { FC, useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Portal: FC<IProps> = ({ children }) => {
+  const el = useRef<Element | DocumentFragment | null>(null);
+
+  if (!el.current) {
+    el.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    const container = document.getElementById("portal-root") as HTMLElement;
+
+    if (el.current) {
+      container.appendChild(el.current);
+    }
+
+    return () => {
+      if (el.current) {
+        container.removeChild(el.current);
+      }
+    };
+  }, []);
+
+  return el.current ? createPortal(children, el.current) : null;
+};
+
+export default Portal;
+
+interface IProps {
+  children: React.ReactNode;
+}
