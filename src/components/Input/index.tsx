d2f00b265de9867d13f660cbcf5020f5edfbd6a8
Steph Crown <emmanuelstephen024@gmail.com>
2023-01-09 19:45:09 +0100

    Create input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react";
+
+const Input: FC<IProps> = ({
+  type,
+  className,
+  withIcon,
+  placeholder,
+  ...rest
+}) => {
+  return (
+    <input
+      type={type || "text"}
+      className={`input ${withIcon ? "icon-input" : ""} `}
+      placeholder={placeholder}
+      {...rest}
+    />
+  );
+};
+
+export default Input;
+
+interface IProps
+  extends React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  withIcon?: boolean;
+}
