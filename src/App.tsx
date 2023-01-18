409e35621f32b754323f7b822bb42d0d16cc6a5c
Steph Crown <emmanuelstephen024@gmail.com>
2023-01-18 19:37:06 +0100

    Create IssueProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import "./App.css";
 import { ThreeBars } from "./assets/svgs";
 import { useThemes } from "./hooks";
 import { Home } from "./pages";
+import { IssuesProvider } from "./providers";
 
 const App = () => {
   const { handleThemeToggle } = useThemes();
 
   return (
     <div>
-      <Home />
+      <IssuesProvider>
+        <Home />
+      </IssuesProvider>
 
       <button className="toggle-btn" onClick={handleThemeToggle}>
         <ThreeBars />
---
+++ src/providers/index.tsx
@@ -0,0 +1,1 @@
+export { default as IssuesProvider } from "./IssuesProvider";
